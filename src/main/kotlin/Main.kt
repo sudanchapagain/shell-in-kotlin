c9a7b2d930d878c8104285e41c2af17106c8db52
Sudan Chapagain <101015050+sudanchapagain@users.noreply.github.com>
2024-08-06 08:09:26 +0545

    pass stage

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,18 @@
 fun main() {
     val recognizedCommands = arrayOf("help", "exit", "ls", "cd", "pwd", "mkdir", "rmdir", "touch", "rm", "cat", "echo", "clear")
-    print("$ ")
-    val command = readln()
-    if (recognizedCommands.contains(command)) {
+
+    while(true){
+      print("$ ")
+      val command = readln()
+
+      if (command == "exit") {
         return
-    } else {
-      println("${command}: command not found")
+      }
+
+      if (recognizedCommands.contains(command)) {
+        return
+      } else {
+        println("${command}: command not found")
+      }
     }
 }
