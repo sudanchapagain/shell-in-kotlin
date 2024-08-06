6b5dbf7bc495c3372406b7114f491677070a8e6a
Sudan Chapagain <101015050+sudanchapagain@users.noreply.github.com>
2024-08-06 09:57:44 +0545

    Update: README

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,41 @@
 fun main() {
-    val recognizedCommands = arrayOf("help", "exit", "ls", "cd", "pwd", "mkdir", "rmdir", "touch", "rm", "cat", "echo", "clear")
+    val recognizedCommands = arrayOf("exit", "echo", "type")
 
+    // REPL loop
     while(true){
-      print("$ ")
-      val command = readln()
+        // prompt symbol
+        print("$ ")
 
-      if (command == "exit") {
-        return
-      }
+        val input = readln().trim()
+        // split the input with <space> as delimiter &
+        // 2 max substrings. 1st for command name, 2nd
+        // for arguments to be passed to commands.
+        val parts = input.split(" ", limit = 2)
+        val command = parts[0]
+        // assign the second input substring to arguments if it exists.
+        val arguments = if (parts.size > 1) parts[1] else ""
 
-      if (recognizedCommands.contains(command)) {
-        return
-      } else {
-        println("${command}: command not found")
-      }
+        if (recognizedCommands.contains(command)) {
+            when (command) {
+                "exit" -> return
+                "echo" -> echo(arguments)
+                "type" -> type(arguments, recognizedCommands)
+            }
+        } else {
+            println("${command}: command not found")
+        }
+    }
+}
+
+private fun echo(arguments: String) {
+    println(arguments)
+}
+
+private fun type(arguments: String, recognizedCommands: Array<String>) {
+    if (arguments.isEmpty()){ return }
+    if (recognizedCommands.contains(arguments)){
+        println("$arguments is a shell builtin")
+    } else {
+        println("$arguments: not found")
     }
 }
