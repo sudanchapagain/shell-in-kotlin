d480f1716d0e4d64e8f70585c51ff589c76a62b1
Sudan Chapagain <101015050+sudanchapagain@users.noreply.github.com>
2024-08-06 12:02:44 +0545

    Add: type builtin (Executable files)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
+import java.io.File
+
 fun main() {
     val recognizedCommands = arrayOf("exit", "echo", "type")
 
@@ -7,8 +9,7 @@ fun main() {
         print("$ ")
 
         val input = readln().trim()
-        // split the input with <space> as delimiter &
-        // 2 max substrings. 1st for command name, 2nd
+        // split the input with <space> as delimiter & 2 max substrings. 1st for command name, 2nd
         // for arguments to be passed to commands.
         val parts = input.split(" ", limit = 2)
         val command = parts[0]
@@ -33,8 +34,34 @@ private fun echo(arguments: String) {
 
 private fun type(arguments: String, recognizedCommands: Array<String>) {
     if (arguments.isEmpty()){ return }
+
+    // fetch PATH environment variable which is a
+    // string containing a list of directories separated by a specific
+    // character (colon : on Unix-like systems and semicolon ; on Windows).
+    val pathEnv = System.getenv("PATH")
+    val pathSeparator = if (System.getProperty("os.name").lowercase().contains("win")) ";" else ":"
+    val paths = pathEnv.split(pathSeparator) // makes substrings of all paths specified by the separator above.
+
+    // firstOrNull function iterates over the collection, applies the lambda to each element,
+    // and returns the first element that matches the condition or null.
+    // Lambda expression makes a file object that take the directory and the file name
+    // to represent target which is matched to test if it exists, is a file, and is an executable.
+    // dir is a placeholder for the item in paths currently being processed.
+    val path = paths.firstOrNull { dir ->
+        val file = File(dir, arguments)
+        file.exists() && file.isFile && file.canExecute()
+    }
+
+    // check if it's shell builtin
     if (recognizedCommands.contains(arguments)){
         println("$arguments is a shell builtin")
+    } else if (path != null) {
+        // print the path of executable argument
+        if (System.getProperty("os.name").lowercase().contains("win")){
+            println("$arguments is located at $path\\$arguments")
+        } else {
+            println("$arguments is not located at $path/$arguments")
+        }
     } else {
         println("$arguments: not found")
     }
