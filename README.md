ef29b95ccea4b099574e8928bfcc1bb425efab83
Sudan Chapagain <101015050+sudanchapagain@users.noreply.github.com>
2026-08-22 22:55:00 +0545

    CODEBERG
    Signed-off-by: Sudan Chapagain <101015050+sudanchapagain@users.noreply.github.com>

--- README.md
+++
@@ -1,3 +0,0 @@
-# Shell in Kotlin
-
-A shell that's capable of running external programs and builtin commands like `cd`, `pwd`, `echo` and more.
--- app/src/main/kotlin/App.kt
+++
@@ -1,8 +0,0 @@
-package np.com.sudanchapagain.app
-
-import np.com.sudanchapagain.utils.Shell
-
-fun main() {
-    val shell = Shell()
-    shell.repl()
-}
